eb928d06de28921d639f39b56747d07b4be0aecb
Adam Daniels <tnblueswirl@gmail.com>
2012-12-21 15:39:21 -0500

    Adding Initial Files

---
+++ app/code/local/AtlanticBT/Advancedconfig/controllers/Adminhtml/Catalog/ProductController.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Catalog product controller
+ *
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+
+/**
+ * Tasks
+ * 
+ * TODO: init config price rules - Populate rules table existing price rules
+ * TODO: detect prices from existing price rules to populate rules table
+ * TODO: save config price rules
+ */
+
+
+include_once('Mage/Adminhtml/controllers/Catalog/ProductController.php');
+
+class AtlanticBT_Advancedconfig_Adminhtml_Catalog_ProductController 
+    extends Mage_Adminhtml_Catalog_ProductController
+{
+    /*public function newConditionHtmlAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+        $typeArr = explode('|', str_replace('-', '/', $this->getRequest()->getParam('type')));
+        $type = $typeArr[0];
+
+        $model = Mage::getModel($type)
+            ->setId($id)
+            ->setType($type)
+            ->setRule(Mage::getModel('catalogrule/rule'))
+            ->setPrefix('conditions');
+        if (!empty($typeArr[1])) {
+            $model->setAttribute($typeArr[1]);
+        }
+
+        if ($model instanceof Mage_Rule_Model_Condition_Abstract) {
+            $model->setJsFormObject($this->getRequest()->getParam('form'));
+            $html = $model->asHtmlRecursive();
+        } else {
+            $html = '';
+        }
+        $this->getResponse()->setBody($html);
+    }*/
+    
+    /**
+     * Initialize product from request parameters
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _initProduct()
+    {
+        $this->_title($this->__('Catalog'))
+             ->_title($this->__('Manage Products'));
+
+        $productId  = (int) $this->getRequest()->getParam('id');
+        $product    = Mage::getModel('catalog/product')
+            ->setStoreId($this->getRequest()->getParam('store', 0));
+
+        if (!$productId) {
+            if ($setId = (int) $this->getRequest()->getParam('set')) {
+                $product->setAttributeSetId($setId);
+            }
+
+            if ($typeId = $this->getRequest()->getParam('type')) {
+                $product->setTypeId($typeId);
+            }
+        }
+
+        $product->setData('_edit_mode', true);
+        if ($productId) {
+            try {
+                $product->load($productId);
+            } catch (Exception $e) {
+                $product->setTypeId(Mage_Catalog_Model_Product_Type::DEFAULT_TYPE);
+                Mage::logException($e);
+            }
+        }
+
+        $attributes = $this->getRequest()->getParam('attributes');
+        if ($attributes && $product->isConfigurable() &&
+            (!$productId || !$product->getTypeInstance()->getUsedProductAttributeIds())) {
+            $product->getTypeInstance()->setUsedProductAttributeIds(
+                explode(",", base64_decode(urldecode($attributes)))
+            );
+        }
+
+        // Required attributes of simple product for configurable creation
+        if ($this->getRequest()->getParam('popup')
+            && $requiredAttributes = $this->getRequest()->getParam('required')) {
+            $requiredAttributes = explode(",", $requiredAttributes);
+            foreach ($product->getAttributes() as $attribute) {
+                if (in_array($attribute->getId(), $requiredAttributes)) {
+                    $attribute->setIsRequired(1);
+                }
+            }
+        }
+
+        if ($this->getRequest()->getParam('popup')
+            && $this->getRequest()->getParam('product')
+            && !is_array($this->getRequest()->getParam('product'))
+            && $this->getRequest()->getParam('id', false) === false) {
+
+            $configProduct = Mage::getModel('catalog/product')
+                ->setStoreId(0)
+                ->load($this->getRequest()->getParam('product'))
+                ->setTypeId($this->getRequest()->getParam('type'));
+
+            /* @var $configProduct Mage_Catalog_Model_Product */
+            $data = array();
+            foreach ($configProduct->getTypeInstance()->getEditableAttributes() as $attribute) {
+                /* @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
+                if(!$attribute->getIsUnique()
+                    && $attribute->getFrontend()->getInputType()!='gallery'
+                    && $attribute->getAttributeCode() != 'required_options'
+                    && $attribute->getAttributeCode() != 'has_options'
+                    && $attribute->getAttributeCode() != $configProduct->getIdFieldName()) {
+                    $data[$attribute->getAttributeCode()] = $configProduct->getData($attribute->getAttributeCode());
+                }
+            }
+
+            $product->addData($data)
+                ->setWebsiteIds($configProduct->getWebsiteIds());
+        }
+
+        Mage::register('product', $product);
+        Mage::register('current_product', $product);
+        Mage::getSingleton('cms/wysiwyg_config')->setStoreId($this->getRequest()->getParam('store'));
+        return $product;
+    }
+    
+    /**
+     * Get super config grid
+     *
+     */
+    public function superConfigAction()
+    {
+        $this->_initProduct();
+        $this->loadLayout(false);
+        $this->renderLayout();
+    }
+    
+    /**
+     * Save product action
+     */
+    public function saveAction()
+    {
+        $storeId        = $this->getRequest()->getParam('store');
+        $redirectBack   = $this->getRequest()->getParam('back', false);
+        $productId      = $this->getRequest()->getParam('id');
+        $isEdit         = (int)($this->getRequest()->getParam('id') != null);
+
+        $data = $this->getRequest()->getPost();
+        if ($data) {
+            $this->_filterStockData($data['product']['stock_data']);
+
+            $product = $this->_initProductSave();
+
+            try {
+                $product->save();
+                $productId = $product->getId();
+
+                /**
+                 * Do copying data to stores
+                 */
+                if (isset($data['copy_to_stores'])) {
+                    foreach ($data['copy_to_stores'] as $storeTo=>$storeFrom) {
+                        $newProduct = Mage::getModel('catalog/product')
+                            ->setStoreId($storeFrom)
+                            ->load($productId)
+                            ->setStoreId($storeTo)
+                            ->save();
+                    }
+                }
+
+                Mage::getModel('catalogrule/rule')->applyAllRulesToProduct($productId);
+
+                $this->_getSession()->addSuccess($this->__('The product has been saved.'));
+            } catch (Mage_Core_Exception $e) {
+                $this->_getSession()->addError($e->getMessage())
+                    ->setProductData($data);
+                $redirectBack = true;
+            } catch (Exception $e) {
+                Mage::logException($e);
+                $this->_getSession()->addError($e->getMessage());
+                $redirectBack = true;
+            }
+        }
+
+        if ($redirectBack) {
+            $this->_redirect('*/*/edit', array(
+                'id'    => $productId,
+                '_current'=>true
+            ));
+        } elseif($this->getRequest()->getParam('popup')) {
+            $this->_redirect('*/*/created', array(
+                '_current'   => true,
+                'id'         => $productId,
+                'edit'       => $isEdit
+            ));
+        } else {
+            $this->_redirect('*/*/', array('store'=>$storeId));
+        }
+    }
+    
+    /**
+     * Initialize product before saving
+     */
+    protected function _initProductSave()
+    {
+        $product     = $this->_initProduct();
+        $productData = $this->getRequest()->getPost('product');
+        if ($productData) {
+            $this->_filterStockData($productData['stock_data']);
+        }
+
+        /**
+         * Websites
+         */
+        if (!isset($productData['website_ids'])) {
+            $productData['website_ids'] = array();
+        }
+
+        $wasLockedMedia = false;
+        if ($product->isLockedAttribute('media')) {
+            $product->unlockAttribute('media');
+            $wasLockedMedia = true;
+        }
+
+        $product->addData($productData);
+
+        if ($wasLockedMedia) {
+            $product->lockAttribute('media');
+        }
+
+        if (Mage::app()->isSingleStoreMode()) {
+            $product->setWebsiteIds(array(Mage::app()->getStore(true)->getWebsite()->getId()));
+        }
+
+        /**
+         * Create Permanent Redirect for old URL key
+         */
+        if ($product->getId() && isset($productData['url_key_create_redirect']))
+        // && $product->getOrigData('url_key') != $product->getData('url_key')
+        {
+            $product->setData('save_rewrites_history', (bool)$productData['url_key_create_redirect']);
+        }
+
+        /**
+         * Check "Use Default Value" checkboxes values
+         */
+        if ($useDefaults = $this->getRequest()->getPost('use_default')) {
+            foreach ($useDefaults as $attributeCode) {
+                $product->setData($attributeCode, false);
+            }
+        }
+
+        /**
+         * Init product links data (related, upsell, crosssel)
+         */
+        $links = $this->getRequest()->getPost('links');
+        if (isset($links['related']) && !$product->getRelatedReadonly()) {
+            $product->setRelatedLinkData(Mage::helper('adminhtml/js')->decodeGridSerializedInput($links['related']));
+        }
+        if (isset($links['upsell']) && !$product->getUpsellReadonly()) {
+            $product->setUpSellLinkData(Mage::helper('adminhtml/js')->decodeGridSerializedInput($links['upsell']));
+        }
+        if (isset($links['crosssell']) && !$product->getCrosssellReadonly()) {
+            $product->setCrossSellLinkData(Mage::helper('adminhtml/js')
+                ->decodeGridSerializedInput($links['crosssell']));
+        }
+        if (isset($links['grouped']) && !$product->getGroupedReadonly()) {
+            $product->setGroupedLinkData(Mage::helper('adminhtml/js')->decodeGridSerializedInput($links['grouped']));
+        }
+
+        /**
+         * Initialize product categories
+         */
+        $categoryIds = $this->getRequest()->getPost('category_ids');
+        if (null !== $categoryIds) {
+            if (empty($categoryIds)) {
+                $categoryIds = array();
+            }
+            $product->setCategoryIds($categoryIds);
+        }
+
+        /**
+         * Initialize data for configurable product
+         */
+        if (($data = $this->getRequest()->getPost('configurable_products_data'))
+            && !$product->getConfigurableReadonly()
+        ) {
+            $product->setConfigurableProductsData(Mage::helper('core')->jsonDecode($data));
+        }
+        if (($data = $this->getRequest()->getPost('configurable_attributes_data'))
+            && !$product->getConfigurableReadonly()
+        ) {
+            $product->setConfigurableAttributesData(Mage::helper('core')->jsonDecode($data));
+        }
+
+        $product->setCanSaveConfigurableAttributes(
+            (bool) $this->getRequest()->getPost('affect_configurable_product_attributes')
+                && !$product->getConfigurableReadonly()
+        );
+
+        /**
+         * Initialize product options
+         */
+        if (isset($productData['options']) && !$product->getOptionsReadonly()) {
+            $product->setProductOptions($productData['options']);
+        }
+
+        $product->setCanSaveCustomOptions(
+            (bool)$this->getRequest()->getPost('affect_product_custom_options')
+            && !$product->getOptionsReadonly()
+        );
+
+        Mage::dispatchEvent(
+            'catalog_product_prepare_save',
+            array('product' => $product, 'request' => $this->getRequest())
+        );
+
+        return $product;
+    }
+}
